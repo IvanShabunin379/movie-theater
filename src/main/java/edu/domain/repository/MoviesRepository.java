9836ba7f60dc999f73287e7b0f8bb790bac72f27
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-05-15 14:34:52 +0300

    add AuditoriumsRepository, MoviesRepository and UsersRepository

---
+++ src/main/java/edu/domain/repository/MoviesRepository.java
@@ -0,0 +1,200 @@
+package edu.domain.repository;
+
+import edu.domain.model.Movie;
+import edu.domain.repository.exception.DataAccessException;
+import edu.domain.repository.mapper.MovieMapper;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class MoviesRepository {
+    private static final String FIND_ALL_TEMPLATE = """
+            SELECT id,
+                   name,
+                   year,
+                   country_id,
+                   poster_path,
+                   genre,
+                   duration,
+                   description,
+                   movie_id,
+                   is_currently_at_box_office
+            FROM movies
+            """;
+    private static final String FIND_ALL_ARE_CURRENTLY_AT_BOX_OFFICE = """
+            SELECT id,
+                   name,
+                   year,
+                   country_id,
+                   poster_path,
+                   genre,
+                   duration,
+                   description,
+                   movie_id,
+                   is_currently_at_box_office
+            FROM movies
+            WHERE is_currently_at_box_office = true     
+            """;
+    private static final String FIND_BY_ID_TEMPLATE = """
+            SELECT id,
+                   name,
+                   year,
+                   country_id,
+                   poster_path,
+                   genre,
+                   duration,
+                   description,
+                   movie_id,
+                   is_currently_at_box_office
+            FROM movies
+            WHERE id = ?
+            """;
+    private static final String SAVE_TEMPLATE = """
+            INSERT INTO movies(name,
+                               year,
+                               country_id,
+                               poster_path,
+                               genre,
+                               duration,
+                               description,
+                               movie_id,
+                               is_currently_at_box_office)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
+    private static final String UPDATE_TEMPLATE = """
+            UPDATE movies
+            SET name = ?,
+                year = ?,
+                country_id = ?,
+                poster_path = ?,
+                genre = ?,
+                duration = ?,
+                description = ?,
+                movie_id = ?,
+                is_currently_at_box_office = ?
+            WHERE id = ?
+            """;
+    private static final String DELETE_TEMPLATE = "DELETE FROM movies WHERE id = ?";
+
+    private Connection connection;
+    private final MovieMapper movieMapper;
+
+    public List<Movie> findAll() {
+        List<Movie> movies = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_TEMPLATE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Movie movie = movieMapper.mapRow(resultSet);
+                movies.add(movie);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return movies;
+    }
+
+    public List<Movie> findAllAreCurrentlyAtBoxOffice() {
+        List<Movie> movies = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_ARE_CURRENTLY_AT_BOX_OFFICE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Movie movie = movieMapper.mapRow(resultSet);
+                movies.add(movie);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return movies;
+    }
+
+    public Optional<Movie> findById(int id) {
+        Optional<Movie> result = Optional.empty();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_TEMPLATE);
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Movie movie = movieMapper.mapRow(resultSet);
+                result = Optional.of(movie);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return result;
+    }
+
+    public boolean save(@NotNull Movie movie) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SAVE_TEMPLATE);
+            preparedStatement.setString(1, movie.getName());
+            preparedStatement.setInt(2, movie.getYear());
+            preparedStatement.setInt(3, movie.getCountryId());
+            preparedStatement.setString(4, movie.getPosterPath());
+            preparedStatement.setObject(5, movie.getGenre());
+            preparedStatement.setInt(6, movie.getDuration());
+            preparedStatement.setString(7, movie.getDescription());
+            preparedStatement.setInt(8, movie.getDirectorId());
+            preparedStatement.setBoolean(9, movie.getIsCurrentlyAtBoxOffice());
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            if (e.getSQLState().equals("23000")) {
+                return false;
+            } else {
+                throw new DataAccessException(e);
+            }
+        }
+    }
+
+    public boolean update(int id, @NotNull Movie updatedMovie) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_TEMPLATE);
+
+            preparedStatement.setString(1, updatedMovie.getName());
+            preparedStatement.setInt(2, updatedMovie.getYear());
+            preparedStatement.setInt(3, updatedMovie.getCountryId());
+            preparedStatement.setString(4, updatedMovie.getPosterPath());
+            preparedStatement.setObject(5, updatedMovie.getGenre());
+            preparedStatement.setInt(6, updatedMovie.getDuration());
+            preparedStatement.setString(7, updatedMovie.getDescription());
+            preparedStatement.setInt(8, updatedMovie.getDirectorId());
+            preparedStatement.setBoolean(9, updatedMovie.getIsCurrentlyAtBoxOffice());
+            preparedStatement.setInt(10, id);
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public boolean delete(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_TEMPLATE);
+            preparedStatement.setInt(1, id);
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+}
---
+++ src/main/java/edu/domain/repository/UsersRepository.java
@@ -0,0 +1,140 @@
+package edu.domain.repository;
+
+import edu.domain.model.User;
+import edu.domain.repository.exception.DataAccessException;
+import edu.domain.repository.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class UsersRepository {
+    private static final String FIND_ALL_TEMPLATE = "SELECT id, name, email, password_hash FROM users";
+    private static final String FIND_BY_ID_TEMPLATE = "SELECT id, name, email, password_hash FROM users WHERE id = ?";
+    private static final String FIND_BY_EMAIL_TEMPLATE = """
+            SELECT id, name, email, password_hash
+            FROM users
+            WHERE email = ?
+            """;
+    private static final String SAVE_TEMPLATE = "INSERT INTO users(name, email, password_hash) VALUES (?, ?, ?)";
+    private static final String UPDATE_TEMPLATE = """
+            UPDATE users 
+            SET name = ?,
+                email = ?,
+                password_hash = ?
+            WHERE id = ?
+            """;
+    private static final String DELETE_TEMPLATE = "DELETE FROM users WHERE id = ?";
+
+    private Connection connection;
+    private final UserMapper userMapper;
+
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_TEMPLATE);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                User user = userMapper.mapRow(resultSet);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return users;
+    }
+
+    public Optional<User> findById(int id) {
+        Optional<User> result = Optional.empty();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID_TEMPLATE);
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                User user = userMapper.mapRow(resultSet);
+                result = Optional.of(user);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return result;
+    }
+
+    public Optional<User> findByEmail(@NotNull String email) {
+        Optional<User> result = Optional.empty();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_EMAIL_TEMPLATE);
+            preparedStatement.setString(1, "email");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                User user = userMapper.mapRow(resultSet);
+                result = Optional.of(user);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+        return result;
+    }
+
+    public boolean save(@NotNull User user) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SAVE_TEMPLATE);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPasswordHash());
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            if (e.getSQLState().equals("23000")) {
+                return false;
+            } else {
+                throw new DataAccessException(e);
+            }
+        }
+    }
+
+    public boolean update(int id, @NotNull User updatedUser) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_TEMPLATE);
+
+            preparedStatement.setString(1, updatedUser.getName());
+            preparedStatement.setString(2, updatedUser.getEmail());
+            preparedStatement.setString(3, updatedUser.getPasswordHash());
+            preparedStatement.setInt(4, id);
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public boolean delete(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DELETE_TEMPLATE);
+            preparedStatement.setInt(1, id);
+
+            return preparedStatement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+}
