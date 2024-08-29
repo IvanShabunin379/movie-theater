e2b75fbd4259f4e310d5b9eb45fd0a2e3a8e3ea7
IvanShabunin379 <IvanShabunin379@yandex.ru>
2024-08-29 15:51:29 +0300

    move db configs to properties config file

--- src/main/java/edu/database/ConnectionFactory.java
+++ src/main/java/edu/database/ConnectionFactory.java
@@ -3,22 +3,28 @@ package edu.database;
 import edu.database.exception.DatabaseConnectionException;
 import lombok.experimental.UtilityClass;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @UtilityClass
 public class ConnectionFactory {
-    private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5432/movie_theater";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
-
     public static Connection getConnection() {
-        try {
-            Class.forName(DRIVER_CLASS_NAME);
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
+        Properties props = new Properties();
+
+        try (InputStream input = ConnectionFactory.class.getClassLoader().getResourceAsStream("db.properties")) {
+            props.load(input);
+
+            Class.forName(props.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    props.getProperty("url"),
+                    props.getProperty("username"),
+                    props.getProperty("password")
+            );
+        } catch (IOException | ClassNotFoundException | SQLException e) {
             throw new DatabaseConnectionException(e);
         }
     }
